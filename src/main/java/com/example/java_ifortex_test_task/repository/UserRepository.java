038da59eb0b6ae11d26c37378258a785ae32410d
Yauheni Lamashkevich <lamashkevich.evg@gmail.com>
2025-06-24 23:18:18 +0300

    Update UserRepository

--- src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
@@ -6,11 +6,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "", nativeQuery = true)
-    List<User> getUsersWithAtLeastOneMobileSession(DeviceType deviceType);
+    @Query(value = """
+        SELECT u.* FROM users u
+        JOIN sessions s ON u.id = s.user_id
+        WHERE u.deleted = false
+        AND s.device_type = :#{#deviceType.code}
+        GROUP BY u.id
+        ORDER BY MAX(s.started_at_utc) DESC
+        """, nativeQuery = true)
+    List<User> getUsersWithAtLeastOneSessionByDeviceType(DeviceType deviceType);
 
-    @Query(value = "", nativeQuery = true)
-    User getUserWithMostSessions();
+    @Query(value = """
+        SELECT u.* FROM users u
+        JOIN sessions s ON u.id = s.user_id
+        WHERE u.deleted = false
+        GROUP BY u.id
+        ORDER BY COUNT(s.id) DESC
+        LIMIT 1
+        """, nativeQuery = true)
+    Optional<User> getUserWithMostSessions();
 }
